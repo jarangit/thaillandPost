93cb22042dcaa8b41d4b38ffce78696e9f684d4a
jarangit <jaran.dch@gmail.com>
2022-11-12 21:26:30 +0700

    first

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className={`flex flex-col justify-center items-center min-h-screen`}>
+      <div>Thailand Post Project</div>
+      <div>Now setting Project...</div>
+    </div>
+  )
+}

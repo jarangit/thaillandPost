d0fe4edaa58b4a319707e8ac3bb1248dd49385d3
jarangit <jaran.dch@gmail.com>
2022-11-14 00:54:26 +0700

    feat: create footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,10 +1,84 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { list1, list2, address } from '../../constant/footerLink'
+import { RiContactsBookFill } from 'react-icons/ri'
+import { MdOutlinePhoneIphone } from 'react-icons/md'
+import { FaFax } from 'react-icons/fa'
+import { AiOutlineMail } from 'react-icons/ai'
 
 type Props = {}
 
 const Footer = (props: Props) => {
+
+  const renderAddressIcon = useCallback((type: string) => {
+    switch (type) {
+      case "address":
+        return <RiContactsBookFill size={20} />
+      case "phone":
+        return <MdOutlinePhoneIphone size={20} />
+      case "fax":
+        return <FaFax size={20} />
+      case "email":
+        return <AiOutlineMail size={20} />
+      default:
+        break;
+    }
+  }, [])
+
   return (
-    <div className={`bg-blue text-white p-3`}>Footer</div>
+    <div className={`bg-blue text-white py-10 px-6 lg:px-3`}>
+      <div className={`myContainer grid grid-cols-1 md:grid-cols-4 gap-10 md:gap-6`}>
+        <div>
+          <div className={`font-semibold`}>ข่าวประชาสัมพันธ์</div>
+          <ul className={`flex flex-col gap-1 mt-4 hover:text-gray`}>
+            {list1.map((item, key) => (
+              <React.Fragment key={key}>
+                <li className={`cursor-pointer hover:text-red`}> {item.name} </li>
+              </React.Fragment>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <div>
+            <div className={`font-semibold`}>แคตตาล็อก</div>
+            <ul className={`flex flex-col gap-1 mt-4 hover:text-gray`}>
+              {list2.map((item, key) => (
+                <React.Fragment key={key}>
+                  <li className={`cursor-pointer hover:text-red`}> {item.name} </li>
+                </React.Fragment>))}
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className={`font-semibold`}>หน่วยงานที่เกี่ยวข้อง</div>
+            <ul className={`flex flex-col gap-1 mt-4 hover:text-gray`}>
+              {list2.map((item, key) => (
+                <React.Fragment key={key}>
+                  <li className={`cursor-pointer hover:text-red`}> {item.name} </li>
+                </React.Fragment>))}
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div className={`font-semibold`}>บริษัท ไปรษณีย์ไทย จำกัด</div>
+            <ul className={`flex flex-col gap-1 mt-4 hover:text-gray`}>
+              {address.map((item, key) => (
+                <React.Fragment key={key}>
+                  <li className={`cursor-pointer hover:text-red flex gap-3`}>
+                    <div className={`mt-1`}>
+                      {renderAddressIcon(item.type)}
+                    </div>
+                    <div>
+                      {item.text}
+                    </div>
+                  </li>
+                </React.Fragment>))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ constant/footerLink.js
@@ -0,0 +1,61 @@
+export const list1 = [
+  {
+    name: 'เงื่อนไขการเป็นสมาชิก',
+    url: '#',
+  },
+  {
+    name: 'ข่าวสารเกี่ยวกับสินค้าและบริการ',
+    url: '#',
+  },
+  {
+    name: `โครงการตราไปรษณียากรและสิ่งสะสมประจำปี`,
+    url: '#',
+  },
+  {
+    name: `การแจ้งเลือนวันแรกจำหน่ายของสินค้า`,
+    url: '#',
+  },
+]
+export const list2 = [
+  {
+    name: 'แคตตาล็อกประจำเดือน มิ.ย. 65',
+    url: '#',
+  },
+  {
+    name: 'แคตตาล็อกประจำเดือน ก.ค. 65',
+    url: '#',
+  },
+]
+export const list3 = [
+  {
+    name: '@Stampinlove',
+    url: '#',
+  },
+  {
+    name: 'Thailand Post Mart',
+    url: '#',
+  },
+]
+export const address = [
+  {
+    text: `111 ถนนแจ้งวัฒนะ แขวงทุ่งสองห้อง เขตหลักสี่ 
+    กรุงเทพมหานคร 10210`,
+    url: '#',
+    type: "address"
+  },
+  {
+    text: '0 2831 3131',
+    url: '#',
+    type: "phone"
+  },
+  {
+    text: `0 2831 3550-1`,
+    url: '#',
+    type: "fax"
+  },
+  {
+    text: `postalcare@thailandpost.co.th`,
+    url: '#',
+    type: 'email'
+  },
+]

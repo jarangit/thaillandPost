1b3778221712826fe821ad77407e7599270681f2
jarangit <jaran.dch@gmail.com>
2022-11-17 13:18:21 +0700

    create mb menu

--- components/layout/menu.tsx
+++ components/layout/menu.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdOutlineKeyboardArrowDown, MdOutlineMenu } from 'react-icons/md'
 import Image from 'next/image'
 import Button from '../button/button'
 import { IoIosHome } from 'react-icons/io'
 import InputSearch from '../input/inputSearch'
 import { AiFillCaretDown, } from 'react-icons/ai'
+import MobileMenu from './mobileMenu'
 type Props = {}
 
-const listMenu = [
+export const listMenu = [
   {
     name: 'ข่าวประชาสัมพันธ์',
     url: '#'
@@ -22,65 +23,69 @@ const listMenu = [
   },
 ]
 const Menu = (props: Props) => {
+  const [showMBMenu, setShowMBMenu] = useState(false)
   return (
-    <div>
-      <div className={`flex justify-between items-center lg:h-24 px-2 lg:px-6 bg-white`}>
-        <div className={`flex gap-3 md:gap-10 items-center`}>
-          <div className={`relative w-12 h-12 md:w-24 md:h-24`}>
-            <Image
-              src='/img/logo.png'
-              alt='logo'
-              fill
-              style={{
-                objectFit: 'contain'
-              }}
-            />
+    <>
+        <MobileMenu onClose = {() => setShowMBMenu(false)} onShow = {showMBMenu}/>
+      <div>
+        <div className={`flex justify-between items-center lg:h-24 px-2 lg:px-6 bg-white`}>
+          <div className={`flex gap-3 md:gap-10 items-center`}>
+            <div className={`relative w-12 h-12 md:w-24 md:h-24`}>
+              <Image
+                src='/img/logo.png'
+                alt='logo'
+                fill
+                style={{
+                  objectFit: 'contain'
+                }}
+              />
+            </div>
+            <div>
+              <ul className={`lg:flex gap-4 hidden`}>
+                {listMenu && listMenu.map((item, key) => (
+                  <React.Fragment key={key}>
+                    <li className={`flex gap-2 items-center cursor-pointer hover:text-blue transition-all`}>
+                      <div>{item.name}</div>
+                      <MdOutlineKeyboardArrowDown />
+                    </li>
+                  </React.Fragment>
+                ))}
+              </ul>
+            </div>
           </div>
+          <div className={` flex flex-row gap-3 items-center`}>
+            <Button>เข้าสู่ระบบ</Button>
+            <Button>ลงทะเบียน</Button>
+            <div className='lg:hidden' onClick={() => setShowMBMenu(true)}>
+              <MdOutlineMenu size={40} color="#002369" />
+            </div>
+          </div>
+        </div>
+        <div className={`bg-red p-2 md:px-6 flex justify-between items-center`}>
           <div>
-            <ul className={`lg:flex gap-4 hidden`}>
-              {listMenu && listMenu.map((item, key) => (
-                <React.Fragment key={key}>
-                  <li className={`flex gap-2 items-center cursor-pointer hover:text-blue transition-all`}>
-                    <div>{item.name}</div>
-                    <MdOutlineKeyboardArrowDown />
-                  </li>
-                </React.Fragment>
-              ))}
-            </ul>
+            <div className={`flex gap-3 items-center text-white`}>
+              <IoIosHome color='#ffff' size={20} />
+              <div className={`text-xs`}>หน้าแรก</div>
+            </div>
           </div>
-        </div>
-        <div className={` flex flex-row gap-3 items-center`}>
-          <Button>เข้าสู่ระบบ</Button>
-          <Button>ลงทะเบียน</Button>
-          <div className='lg:hidden'>
-            <MdOutlineMenu size={40} color="#002369" />
-          </div>
-        </div>
-      </div>
-      <div className={`bg-red p-2 md:px-6 flex justify-between items-center`}>
-        <div>
-          <div className={`flex gap-3 items-center text-white`}>
-            <IoIosHome color='#ffff' size={20} />
-            <div className={`text-xs`}>หน้าแรก</div>
-          </div>
-        </div>
-        <div className={`flex  gap-2 md:gap-6 items-center cursor-pointer`}>
-          <InputSearch />
-          <div className={`text-white flex gap-1 items-center text-xs`}>
-            <Image
-              src='/img/icons/thailand.svg'
-              alt=''
-              width={20}
-              height={20}
-            />
-            <div className={`hidden md:block`}>ภาษาไทย</div>
-            <div className={` hidden md:block`}>
-              <AiFillCaretDown size={10} color="#ffff" />
+          <div className={`flex  gap-2 md:gap-6 items-center cursor-pointer`}>
+            <InputSearch />
+            <div className={`text-white flex gap-1 items-center text-xs`}>
+              <Image
+                src='/img/icons/thailand.svg'
+                alt=''
+                width={20}
+                height={20}
+              />
+              <div className={`hidden md:block`}>ภาษาไทย</div>
+              <div className={` hidden md:block`}>
+                <AiFillCaretDown size={10} color="#ffff" />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
---
+++ components/layout/mobileMenu.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import Button from '../button/button'
+import { listMenu } from './menu'
+import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+
+type Props = {
+  onClose: () => void;
+  onShow: boolean;
+}
+
+const MobileMenu = ({ onClose, onShow }: Props) => {
+  return (
+    <>
+      {onShow && <div className={`absolute bg-gray-dark h-screen w-screen z-10 opacity-70`} onClick={onClose} />
+      }
+      <div className={` transition-all right-0 absolute z-10 bg-white h-screen `}
+        style={{
+          width: `${onShow ? "75%" : "0%"}`,
+          overflow: `${onShow ? "block" : "hidden"}`,
+        }}
+      >
+        <div className={`absolute top-3 right-3`} onClick={onClose}>
+          <BsFillArrowRightCircleFill size={30} color="#D30D2B" />
+        </div>
+        <div className={`flex flex-col gap-3 px-3 py-6`}>
+          <div>เมนู</div>
+          {listMenu && listMenu.map((item, key) => (
+            <React.Fragment key={key} >
+              <li className={`flex gap-2 items-center cursor-pointer hover:text-blue transition-all`}>
+                <div>{item.name}</div>
+                <div className={`absolute right-2`}>
+                  <MdOutlineKeyboardArrowDown />
+                </div>
+              </li>
+            </React.Fragment>
+          ))}
+          <div className={`flex flex gap-3 justify-center`}>
+            <Button>เข้าสู่ระบบ</Button>
+            <Button>ลงทะเบียน</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MobileMenu

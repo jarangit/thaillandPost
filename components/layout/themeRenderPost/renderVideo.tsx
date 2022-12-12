2b1c0b6a5f7f53e431f0e2eddf7510bb59749ce2
jarangit <jaran.dch@gmail.com>
2022-12-12 10:13:54 +0700

    feat: create post type video

--- components/layout/themeRenderPost/renderVideo.tsx
+++ components/layout/themeRenderPost/renderVideo.tsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
+// Render a YouTube video player
 type Props = {
-  data:any;
+  data: any;
 }
 
-const RenderVideo = (props: Props) => {
+const RenderVideo = ({ data }: Props) => {
+  const { title, contents } = data
   return (
-    <div>RenderVideo</div>
+    <div>
+      <div className={`text-blue text-2xl lg:text-3xl`}>ข่าวสารเกี่ยวกับสินค้าและบริการ</div>
+
+      {/* content */}
+      <div className={`mt-6`}>
+        <div className='text-xl lg:text-3xl'>{title}</div>
+        <div className='flex justify-center items-center my-6'>
+          <ReactPlayer
+            url='https://youtu.be/mQtXvO66Zrg'
+            // width={"100%"}
+            // height={"500px"}
+            light={true}
+          />
+        </div>
+        <div className=''>{contents}</div>
+      </div>
+    </div>
   )
 }
 
